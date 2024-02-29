5458face416b8958dcf965f01ee6009cd8e85ab2
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2024-02-29 22:27:04 +0530

    added logic to implement location detection

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Spinner from './Spinner';
 
 const Map = () => {
+
+  const [location, setLocation] = React.useState([0,0]);
+
+  const getLocation = () => {
+    const success = (position) => {
+      setLocation([position.coords.latitude, position.coords.longitude]);
+    };
+    const failure = () => {
+      alert("Unable to retrieve your location or permission denied.");
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(success, failure);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+
+  React.useEffect(() => getLocation(), [location]);
+
+  console.log("location: ", location)
+
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+    // render map container when location is available
+      location[0] === 0 ? <Spinner />:(
+            <MapContainer
+      center={location}
+      zoom={10}
       style={{ height: '50vh', width: '100%' }}
     >
       <TileLayer
         url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=knsMPDdvtrm6AQO31Kfq"
         attribution='&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> contributors'
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={location}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
+      )
   );
 };
 
